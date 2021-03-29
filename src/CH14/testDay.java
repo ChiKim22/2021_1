4b0d17f05c15ca97bd38655859a0dd4089b37b4f
JinhoKim <wlsgh2505@gmail.com>
2021-03-29 19:27:29 +0900

    Assert

--- src/CH14/testDay.java
+++ src/CH14/testDay.java
@@ -9,7 +9,7 @@ public class testDay {
 		System.out.println("날짜 입력");
 		int date = input.nextInt();
 
-		assert (date >= 1 && date <= 31): "잘못된 날짜 : " + date;
+		assert (date >= 1 && date <= 31): "잘못된 날짜 : " + date
 
 		//assert 를 통과하면 실행.
 		System.out.printf("입력된 날짜는 %d 입니다. \n", date);
